671dc55c059812fafd30ca518617137df7dd5d7a
elstr <eleonora.lester@gmail.com>
2021-12-09 07:14:05 -0300

    test

--- src/handlers/lambda-2.js
+++ src/handlers/lambda-2.js
@@ -1,6 +1,9 @@
 exports.handler =  async function(event, context) {
-	console.log('NODE_ENV', process.env.NODE_ENV,)
-	console.log('region', process.env.AWS_REGION,)
-	console.log('accessKeyId', process.env.AWS_ACCESS_KEY_ID,)
-	console.log('secretAccessKey', process.env.AWS_SECRET_ACCESS_KEY)
+	console.log('LLEGO A LA LAMBDA --------')
+	console.log('NODE_ENV', process.env.NODE_ENV)
+	console.log('AWS_ACCESS_KEY_ID',process.env.AWS_ACCESS_KEY_ID)
+	console.log('AWS_REGION',process.env.AWS_REGION)
+	console.log('AWS_SECRET_ACCESS_KEY',process.env.AWS_SECRET_ACCESS_KEY)
+	console.log('DB_HOST',process.env.DB_HOST)
+	console.log('DB_USER',process.env.DB_USER)
 }
